c342b654b60ccb83d8f536eea0b2faed56430c15
Pete Seeling <peteseeling@gmail.com>
2022-02-07 17:07:25 -0600

    env changed to proper db. BBpage workd on.

--- src/BeanieBaby.js
+++ src/BeanieBaby.js
@@ -2,7 +2,16 @@ import { Link } from 'react-router-dom';
 
 export default function BeanieBaby({ beanieBaby }) {
   return (
-    <Link to="/BeanieDetail"></Link>
+    <><div>
+      <img src={beanieBaby.image} />
+      <p>{beanieBaby.name}</p>
+     
+
+      <Link to="/BeanieDetail"></Link>
+    </div><div className='beanie-details'>
+
+
+    </div></>
     // this should contain a react-router-dom Link to the detail page for this particular beanie baby.
     // it should also render the beanie baby's image and show the beanie baby's name
    

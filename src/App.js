03162f01f42e30250b2193748123c1c64f122a02
Pete Seeling <peteseeling@gmail.com>
2022-02-07 14:31:39 -0600

    route/switch added on app.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
+  Link
 } from 'react-router-dom';
 import BeaniesPage from './BeaniesPage';
 import BeanieDetail from './BeanieDetail';
@@ -10,12 +11,21 @@ import BeanieDetail from './BeanieDetail';
 export default function App() {
   return (
     <Router>
+      <ul>
+        <li>
+          <Link to="/BeaniesPage">Beanies Page</Link>
+        </li>
+        <li>
+          <Link to="/BeanieDetail">Beanie Detail Page</Link>
+        </li>
+      </ul>
       <div className='App'>
         <Switch>
-          <Route>
+          <Route path="/BeaniesPage">
+            <BeaniesPage />
             {/* this home page route should list all the beanies */}
           </Route>
-          <Route> 
+          <Route path="/BeanieDetail"> 
             {/* this route should point to a particulat beanie baby by id and render that specific BeanieDetail page */}
           </Route>
         </Switch>

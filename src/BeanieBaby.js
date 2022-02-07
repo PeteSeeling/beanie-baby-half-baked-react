5ae84d3a19f025ebc83f8cf9c4bda9f954e530fd
Pete Seeling <peteseeling@gmail.com>
2022-02-07 15:02:27 -0600

    beanie baby . js link added to page. Appears Wrkng

--- src/BeanieBaby.js
+++ src/BeanieBaby.js
@@ -2,8 +2,9 @@ import { Link } from 'react-router-dom';
 
 export default function BeanieBaby({ beanieBaby }) {
   return (
+    <Link to="/BeanieDetail"></Link>
     // this should contain a react-router-dom Link to the detail page for this particular beanie baby.
     // it should also render the beanie baby's image and show the beanie baby's name
-    null
+   
   );
 }
